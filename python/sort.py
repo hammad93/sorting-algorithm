ae77904d005d0c0ed4274cd9fb3da121fbdb542e
Hammad Usmani <ham.u@live.com>
2019-10-01 21:15:41 -0400

    Update sort.py
    I guess I finally got to it?

--- python/sort.py
+++ python/sort.py
@@ -1,27 +1,14 @@
-'''
-NOTE: python has very slow for loops which make this algorithm mush slower than
-it is. 
-'''
 import math
-import time
 
-def regSort(array) :
-    return array.sort()
-def keySort(array) :
+def keySort(array):
+    # generate key 
     key = 0;
-    for x in array :
-        key += 2**x
-    '''
+    for x in array:
+        key += 2 ** x
+    
+    # sort using key
     sort = []
-    for i in range(len(array)) :
+    for i in range(len(array)):
         value = math.floor(math.log(key, 2))
-        key -= 2**value
-    '''
-array = [12, 3, 4, 5, 6, 2, 9, 11, 10, 45, 234, 12, 78]
-
-start = time.time()
-regSort(array)
-print (time.time() - start)
-start = time.time()
-keySort(array)
-print (time.time() - start)
+        sort.append(value) # highest value in array
+        key -= 2 ** value
